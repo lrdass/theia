dac7a7fdb270c2f4ce912c3addf6aa7294ac1e9b
lrdass <lsuppes@pm.me>
2020-09-15 16:26:42 -0300

    1d range search working

--- data_structures/range_tree/range_tree.py
+++ data_structures/range_tree/range_tree.py
@@ -1,5 +1,31 @@
-from data_structures.utils.auxiliary import Interval
 import pprint
+import math
+
+class Interval:
+    class Range:
+        def __init__(self, min_value, max_value):
+            self.min = min_value
+            self.max = max_value     
+    x = None
+    y = None
+    
+    def __init__(self, x_range=(-math.inf,math.inf), y_range=(-math.inf,math.inf)):
+        self.x = self.Range(min(x_range), max(x_range))
+        self.y = self.Range(min(y_range), max(y_range))
+    
+    def __repr__(self):
+        return 'x : [{},{}], y :[{}, {}]'.format(self.x.min, self.x.max, self.y.min, self.y.max)
+    
+    def __getitem__(self, name):
+        return getattr(self, name)
+    def __setitem__(self, name, value):
+        return setattr(self, name, value)
+    def __delitem__(self, name):
+        return delattr(self, name)
+    def __contains__(self, name):
+        return hasattr(self, name)
+
+
 
 class Node:
     left = None
@@ -60,12 +86,48 @@ def build_binary_tree(points=[]):
         return no
 
 def search_in_range_1d(tree, range=Interval):
-    split_node = find_split_node(tree, range) 
-    pass
+    split = find_split_node(tree, range) 
+    inside = [] 
+
+    if split.is_leaf():
+        if range.x.max <= split.value <= range.x.min:
+            inside.append(split.value)
+    else:
+        no = split.left
+        while not no.is_leaf():
+            if range.x.min <= no.value:
+                inside.extend(report_subtree(node=no.right))
+                no = no.left
+            else:
+                no = no.right 
+        if range.x.min < no.value <= range.x.max:
+            inside.append(no.value)
+
+        no = split.right
+        while not no.is_leaf():
+            if range.x.max > no.value:
+                inside.extend(report_subtree(node=no.left))
+                no = no.right
+            else:
+                no = no.left 
+        if range.x.min < no.value <= range.x.max:
+            inside.append(no.value)
+        
+    return set(inside)
+        
+
+def report_subtree(node=Node, points=[]):
+    if node.is_leaf():
+        points.append(node.value)
+    else:
+        report_subtree(node.left, points)
+        report_subtree(node.right, points)
+    
+    return set(points)
 
 def find_split_node(node=Node, range=Interval):
     no = node
-    while not no.is_leaf and range.x.min<= no.value < range.x.max:
+    while not no.is_leaf and range.x.max<= no.value  or range.x.min > no.value:
         if range.x.max <= no.value :
             no = node.left
         else:
@@ -73,7 +135,7 @@ def find_split_node(node=Node, range=Interval):
 
     return no
 
-arvore = build_binary_tree2([-10, -8, -7, -5, -2, 2, 5, 6, 10, 4, 55,3,5])
-search_in_range_1d(arvore, Interval((-4, 20)))
+arvore = build_binary_tree2([-10, -8, -7, -5, -2, 2, 5, 6])
+print(search_in_range_1d(arvore, Interval((-6, 20))))
 
-print(str(arvore))
+# print(str(arvore))
