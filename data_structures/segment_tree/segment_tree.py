f75b2db0e8e9f85c79cf8a4dc511fa6c453ce34b
lrdass <lsuppes@pm.me>
2021-03-17 13:10:31 -0300

    added a todo list

--- data_structures/segment_tree/segment_tree.py
+++ data_structures/segment_tree/segment_tree.py
@@ -2,6 +2,31 @@ import math
 from random import randint
 import pprint
 
+class Node:
+    left = None
+    right = None
+    value = None
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
+    def __str__(self, level=0):
+        ret = '\t'*level+repr(self.value)+'\n'
+        if not self.is_leaf():
+            ret += '{}'.format(self.right.__str__(level+1))
+            ret += '{}'.format(self.left.__str__(level+1))
+            return ret
+        return ret
+    
+    def __repr__(self):
+        return '{}'.format(self.value)
+
+
 # rework on interval class to represent mathematical intervals
 class Interval:
     left = None
@@ -34,30 +59,34 @@ class Interval:
         return False 
 
 
-segments = [Interval(-6, -5, 'both'), Interval(-2, 1, 'both'), Interval(0, 2, 'both'), Interval(3, 6, 'both'), ]
 ## one dimensional segment tree
-ordered_segments = sorted(segments, key=lambda seg: seg.left)
-elementary_segments = set() 
-for index, segment in enumerate(ordered_segments):
-    # return a list of elementary segments    
-    # ] -inf, first[ ; [first, first] ... so on
-    if index == 0:
-        elementary_segments.add(Interval(-math.inf, segment.left, 'neither'))
-    elif index == len(ordered_segments) - 1:
-        previous_segment = ordered_segments[index-1]
-        elementary_segments.add(Interval(previous_segment.right, segment.left,  'neither'))
-        elementary_segments.add(Interval(segment.right,math.inf, 'neither'))
-    else:
-        previous_segment = ordered_segments[index-1]
-        elementary_segments.add(Interval(previous_segment.right, segment.left,  'neither'))
+def build_elementary_segments(segments=[Interval]):
+    ordered_segments = sorted(segments, key=lambda seg: seg.left)
+    elementary_segments = set() 
+    for index, segment in enumerate(ordered_segments):
+        # return a list of elementary segments    
+        # ] -inf, first[ ; [first, first] ... so on
+        if index == 0:
+            elementary_segments.add(Interval(-math.inf, segment.left, 'neither'))
+        elif index == len(ordered_segments) - 1:
+            previous_segment = ordered_segments[index-1]
+            elementary_segments.add(Interval(previous_segment.right, segment.left,  'neither'))
+            elementary_segments.add(Interval(segment.right,math.inf, 'neither'))
+        else:
+            previous_segment = ordered_segments[index-1]
+            elementary_segments.add(Interval(previous_segment.right, segment.left,  'neither'))
+            
+        elementary_segments.add(Interval(segment.left, segment.left, 'both'))
+        elementary_segments.add(Interval(segment.left, segment.right, 'neither'))
+        elementary_segments.add(Interval(segment.right, segment.right, 'both'))
+    return elementary_segments
 
-    elementary_segments.add(Interval(segment.left, segment.left, 'both'))
-    elementary_segments.add(Interval(segment.left, segment.right, 'neither'))
-    elementary_segments.add(Interval(segment.right, segment.right, 'both'))
-
-
+segments = [Interval(-6, -5, 'both'), Interval(-2, 1, 'both'), Interval(0, 2, 'both'), Interval(3, 6, 'both'), ]
+elementary_segments = build_elementary_segments(segments)
 sorted_ele_segments= sorted(elementary_segments, key=lambda seg: seg.left)
 
+#TODO - Intervals operations that going to be used: in, union, intersect
+#TODO - build tree
 
 sorted_ele_segments= sorted(elementary_segments, key=lambda seg: seg.left)
 print(sorted_ele_segments)
